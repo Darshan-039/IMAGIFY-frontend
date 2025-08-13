ee935f8f61a72476c3fb60c222f7c36294cfdf26
Darshan-039 <darshanmangukiya95@gmail.com>
2025-08-13 19:57:32 +0530

    Added refund policy link in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,6 +9,15 @@ const Footer = () => {
 
             <p className='flex-1 mt-3 border-l pl-4 text-sm text-gray-500 max-sm:hidden'>All right reserved. Copyright @imagify</p>
 
+                <a
+                    href="https://merchant.razorpay.com/policy/R2encVHzX96nmR/refund"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className=" px-4 py-2 rounded-lg bg-gray-800 text-white hover:bg-gray-900 transition-colors duration-200 shadow"
+                >
+                    Refund Policy
+                </a>
+
             <div className='flex gap-2.5'>
                 <img src={assets.facebook_icon} alt="" width={35} />
                 <img src={assets.twitter_icon} alt="" width={35} />

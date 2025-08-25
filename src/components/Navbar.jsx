9d62490b67f846406b41327ecf020723028c0a72
Darshan-039 <darshanmangukiya95@gmail.com>
2025-08-25 20:33:09 +0530

    Removing images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,14 +39,7 @@ const Navbar = () => {
                             <p className='text-xs sm:text-medium text-gray-600'>Credits Left : {credits}</p>
                         </button>
 
-                        {/* {hasImages && */}
-                            <button
-                                onClick={() => navigate('/my-images')}
-                                className="flex items-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700"
-                            >
-                                <p className='text-sm sm:text-medium text-gray-600'>Generated Images</p>
-                            </button>
-                        {/* } */}
+                        
 
 
                         <p className='text-gray-600 max-sm:hidden pl-4'>Hi, {user.name}</p>
